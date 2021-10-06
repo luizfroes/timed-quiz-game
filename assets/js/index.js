00604b8236b8ce4ac65845a536a0e10cc31b332d
Luiz Carlos Froes <luizfroes@gmail.com>
2021-10-06 02:51:48 +0100

    Fixed the objects

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,84 +1,84 @@
 // Questions Objects
 const question1 = {
   question: "What British rock band pioneered the use of the light show?",
-  answer1: "The Beatles",
-  answer2: "The Who",
-  answer3: "Led Zeppelin",
-  answer4: "Pink Floyd",
+  answers: ["The Beatles", "The Who", "Led Zeppelin", "Pink Floyd"],
+  correctAnswer: "Pink Floyd",
 };
 
 const question2 = {
   question: "Who was the first rock-and-roll superstar?",
-  answer1: "Clyde McPhatter",
-  answer2: "Madonna",
-  answer3: "Elvis Presley",
-  answer4: "George Harrison",
+  answers: ["Clyde McPhatter", "Madonna", "Elvis Presley", "George Harrison"],
+  correctAnswer: "Elvis Presley",
 };
 
 const question3 = {
   question: "What are the instruments in a classic three-piece rock band?",
-  answer1: "Guitar, bass, drums",
-  answer2: "Guitar, violin, drums",
-  answer3: "Guitar, bass, trumpet",
-  answer4: "Guitar, organ, drums",
+  answers: [
+    "Guitar, bass, drums",
+    "Guitar, violin, drums",
+    "Guitar, bass, trumpet",
+    "Guitar, organ, drums",
+  ],
+  correctAnswer: "Guitar, bass, drums",
 };
 
 const question4 = {
   question:
     "The famous British rock group Led Zeppelin was initially known as:",
-  answer1: "Acca Dacca",
-  answer2: "The New Yardbirds",
-  answer3: "The Black Album",
-  answer4: "Hollywood Rose",
+  answers: [
+    "Acca Dacca",
+    "The New Yardbirds",
+    "The Black Album",
+    "Hollywood Rose",
+  ],
+  correctAnswer: "The New Yardbirds",
 };
 
 const question5 = {
   question:
     "In which year was Iggy and the Stooges inducted into the Rock and Roll Hall of Fame?",
-  answer1: "1975",
-  answer2: "2010",
-  answer3: "2003",
-  answer4: "1998",
+  answers: ["1975", "2010", "2003", "1998"],
+  correctAnswer: "2010",
 };
 
 const question6 = {
   question: "Which member of the Beatles was known as “the quiet Beatle?",
-  answer1: "Pete Best",
-  answer2: "Ringo Starr",
-  answer3: "George Harrison",
-  answer4: "John Lennon",
+  answers: ["Pete Best", "Ringo Starr", "George Harrison", "John Lennon"],
+  correctAnswer: "George Harrison",
 };
 
 const question7 = {
   question: "In which year did Psychedelic rock emerge?",
-  answer1: "1966",
-  answer2: "1950",
-  answer3: "1970",
-  answer4: "1975",
+  answers: ["1966", "1953", "1972", "1985"],
+  correctAnswer: "1966",
 };
 
 const question8 = {
   question: "Lite metal and Death metal are sub-genres of:",
-  answer1: "Rock",
-  answer2: "Alternative Rock",
-  answer3: "Heavy metal",
-  answer4: "Trip-hop",
+  answers: ["Rock", "Alternative Rock", "Heavy metal", "Trip-hop"],
+  correctAnswer: "Heavy metal",
 };
 
 const question9 = {
   question: "What is the original name of American rock musician Alice Cooper?",
-  answer1: "Steven Tallarico",
-  answer2: "Eric Patrick Clapton",
-  answer3: "Gordon Sumner",
-  answer4: "Vincent Damon Furnier",
+  answers: [
+    "Steven Tallarico",
+    "Eric Patrick Clapton",
+    "Gordon Sumner",
+    "Vincent Damon Furnier",
+  ],
+  correctAnswer: "Vincent Damon Furnier",
 };
 
 const question10 = {
   question: "The 1991 album Nevermind is associated with which rock group?",
-  answer1: "Grateful Dead",
-  answer2: "Nirvana",
-  answer3: "The Ramones",
-  answer4: "The Velvet Underground",
+  answers: [
+    "Grateful Dead",
+    "Nirvana",
+    "The Ramones",
+    "The Velvet Underground",
+  ],
+  correctAnswer: "Nirvana",
 };
 
 //Questions Array
